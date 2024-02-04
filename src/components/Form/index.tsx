610bb3cbedbe6cd619fc45703f8d105dac1808e3
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-02-04 15:46:51 -0300

    feat: complete the form component with basic react hook form features

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,3 +1,24 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface FormFields {
+	name: string;
+	email: string;
+	password: string;
+}
+
 export function Form() {
-	return <form></form>;
+	const { register, handleSubmit } = useForm<FormFields>();
+	const onFormSubmit: SubmitHandler<FormFields> = (data) => {
+		console.log(data);
+	};
+
+	return (
+		<form onSubmit={handleSubmit(onFormSubmit)}>
+			<input type="text" {...register('name')} placeholder="Name" />
+			<input type="email" {...register('email')} placeholder="Email" />
+			<input type="password" {...register('password')} placeholder="Password" />
+
+			<button type="submit">Save</button>
+		</form>
+	);
 }
